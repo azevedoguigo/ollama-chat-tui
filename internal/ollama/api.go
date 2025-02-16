a52ee8bfed29e75249cf64639f5850ae909d49dd
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2025-02-16 01:45:07 -0300

    :sparkles: feat: ollama api comunication.

---
+++ internal/ollama/api.go
@@ -0,0 +1,57 @@
+package ollama
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/azevedoguigo/ollama-chat-tui/internal/storage"
+)
+
+type OllamaRequest struct {
+	Model    string            `json:"model"`
+	Prompt   string            `json:"prompt"`
+	Stream   bool              `json:"stream"`
+	Messages []storage.Message `json:"messages"`
+}
+
+func QueryOllamaStream(messages []storage.Message, callbalc func(string)) error {
+	requestData := OllamaRequest{
+		Model:    "deepseek-r1",
+		Messages: messages,
+		Stream:   true,
+	}
+
+	requestBody, err := json.Marshal(requestData)
+	if err != nil {
+		return err
+	}
+
+	response, err := http.Post(
+		"http://localhost:11434/api/chat",
+		"application/json",
+		bytes.NewBuffer(requestBody),
+	)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	scanner := bufio.NewScanner(response.Body)
+	for scanner.Scan() {
+		var data map[string]interface{}
+
+		if err := json.Unmarshal(scanner.Bytes(), &data); err != nil {
+			return err
+		}
+
+		if message, ok := data["message"].(map[string]interface{}); ok {
+			if content, ok := message["content"].(string); ok {
+				callbalc(content)
+			}
+		}
+	}
+
+	return scanner.Err()
+}
